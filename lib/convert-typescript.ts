592552c8f6b738dadf5cc80d460582b30860eb16
Lenard Pratt <lenard.pratt@ovoenergy.com>
2022-04-07 16:41:56 +0100

    Update deps

--- lib/convert-typescript.ts
+++ lib/convert-typescript.ts
@@ -1,53 +1,42 @@
 import {
-	convertCoreTypesToTypeScript,
-	convertTypeScriptToCoreTypes,
-	FromTsOptions,
-	ToTsOptions,
-} from 'core-types-ts'
+  convertCoreTypesToTypeScript,
+  convertTypeScriptToCoreTypes,
+  FromTsOptions,
+  ToTsOptions,
+} from "@lapz/core-types-ts";
 
-import { Reader } from "./reader"
-import { Writer } from "./writer"
-import { userPackage, userPackageUrl } from "./package"
-import { registerReader, registerWriter } from './format-graph'
+import { Reader } from "./reader";
+import { Writer } from "./writer";
+import { userPackage, userPackageUrl } from "./package";
+import { registerReader, registerWriter } from "./format-graph";
 
-
-export function getTypeScriptReader( tsOptions?: FromTsOptions ): Reader
-{
-	return {
-		kind: 'ts',
-		read( source, { warn, filename } )
-		{
-			return convertTypeScriptToCoreTypes(
-				source,
-				{
-					warn,
-					...tsOptions,
-				}
-			);
-		}
-	};
+export function getTypeScriptReader(tsOptions?: FromTsOptions): Reader {
+  return {
+    kind: "ts",
+    read(source, { warn, filename }) {
+      return convertTypeScriptToCoreTypes(source, {
+        warn,
+        ...tsOptions,
+      });
+    },
+  };
 }
 
-export function getTypeScriptWriter( tsOptions?: ToTsOptions ): Writer
-{
-	return {
-		kind: 'ts',
-		write( doc, { warn, filename, sourceFilename } )
-		{
-			return convertCoreTypesToTypeScript(
-				doc,
-				{
-					warn,
-					filename,
-					sourceFilename,
-					userPackage,
-					userPackageUrl,
-					...tsOptions,
-				}
-			);
-		}
-	};
+export function getTypeScriptWriter(tsOptions?: ToTsOptions): Writer {
+  return {
+    kind: "ts",
+    write(doc, { warn, filename, sourceFilename }) {
+      return convertCoreTypesToTypeScript(doc, {
+        warn,
+        filename,
+        sourceFilename,
+        userPackage,
+        userPackageUrl,
+        ...tsOptions,
+      });
+    },
+  };
 }
 
-registerReader( getTypeScriptReader( ) );
-registerWriter( getTypeScriptWriter( ) );
+registerReader(getTypeScriptReader());
+registerWriter(getTypeScriptWriter());
